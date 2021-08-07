6d5d6b0a3acaab28fa4d3478ba231cec2578ff92
Sebastian Wittenkamp <sebastian@bitops.io>
2021-08-06 22:27:03 -0700

    add module export and http verb handling

--- netlify/functions/echo.js
+++ netlify/functions/echo.js
@@ -1,7 +1,20 @@
 
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({message: "Hello World"})
-  };
-}
+module.exports.handler = async (event, context) => {
+  switch (event.httpMethod) {
+    case "GET":
+      return {
+        statusCode: 200,
+        body: JSON.stringify({message: "get received"})
+      }
+    case "POST":
+      return {
+        statusCode: 200,
+        body: JSON.stringify({message: "post received"})
+      }
+    default:
+      return {
+        statusCode: 400,
+        body: JSON.stringify({message: "not found"})
+      }
+  }
+};
